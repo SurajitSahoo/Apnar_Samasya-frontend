e85066caaf3bae6da99dad0ec1ae07a35c7977d2
Surajit Sahoo <surajitsahoo200@gmail.com>
2026-08-19 22:50:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
+  server: {
+    host: '0.0.0.0',
+
+    allowedHosts: [
+      'apnar-samasya-frontend-latest-1.onrender.com',
+      'apnar-samasya-frontend-latest.onrender.com'
+    ]
+  }
 })
